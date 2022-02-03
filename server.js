b102281e1ea74b78434a7500f36ca7c836762e0a
louvang <louvangdev@gmail.com>
2022-02-02 21:34:08 -0600

    Fix mongoose connection and cleanup modules

--- server.js
+++ server.js
@@ -13,17 +13,13 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5000',
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
